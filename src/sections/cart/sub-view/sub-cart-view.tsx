003bf5e2af60aa729efa86543278cedb726881d3
vietducITITIU19103 <ducnguyenviet91313@gmail.com>
2024-01-10 23:48:06 +0700

    update code

--- src/sections/cart/sub-view/sub-cart-view.tsx
+++ src/sections/cart/sub-view/sub-cart-view.tsx
@@ -8,15 +8,16 @@ import Box from '@mui/material/Box'
 import { useConvertCurrency } from '@/hooks/use-convert-currency'
 import PricingBox from '../components/pricing-box'
 import PaymentMethods from '../components/payment-methods'
+import { TableItemTypes } from '../cart-view'
 
 
-export default function SubCartView() {
+export default function SubCartView({tableData}: {tableData:TableItemTypes[]}) {
     const price = useMemo(() => useConvertCurrency(56000), [])
     return (
         <Container sx={{ backgroundColor: {xs:"iub.background.body",md:'iub.background.default'}, p:{xs:"35px 0",md:"35px"} , my: "30px", borderRadius: "16px", height: "auto" }}>
             <Box mb={{xs: "12px",md:"36px"}} mt="10px">
                 <CartSubTitle sx={{color: "iub.text.light"}}>Danh sách mua</CartSubTitle>
-                <CartTable />
+                <CartTable tableData={tableData}/>
             </Box>
             <Stack direction={{xs: "column", md:"row"}} justifyContent="space-between" alignItems="flex-start" spacing="20px">
                 <PaymentMethods/>
